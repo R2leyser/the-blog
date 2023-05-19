0920d7373b0ad6d30f6a6de4a797ffc074a5748a
R2leyser <51343917+R2leyser@users.noreply.github.com>
2023-05-19 17:19:26 -0300

    create blog index

--- blog/index.js
+++
@@ -1,6 +0,0 @@
-fetch('./blog/template.html')
-    .then(function(res) {
-        var body = res.text();
-        var title = body.split('<title>')[1].split('</title>')[0]
-        console.log(title)
-    })
